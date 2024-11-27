358ed917c03192f27dd9ce3876db8d34a8a708d9
pop <popramel25@gmail.com>
2024-11-27 21:42:35 +0900

    fix: 피드리액션 누구한테 왔는지 나타내기 피드에 리액션 표시

--- models/Feed.js
+++ models/Feed.js
@@ -1,27 +1,44 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// reactions를 위한 하위 스키마 정의
+const reactionSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  type: String,
+  emoji: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//피드
 const feedSchema = new Schema({
-    feedId: { type: String, required: true, unique: true },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId, //String 대신 ObjectId 사용
-      required: true,
-      ref: "User",
-    },
-    content: { type: String, required: true },
-    groupId: String,
-    images: [{ url: String, isAIGenerated: Boolean }],
-    status: { type: String, default: "active" },
-    commentCount: { type: Number, default: 0 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-  });
-  
-  // 인덱스 추가
-  feedSchema.index({ feedId: 1 });
-  feedSchema.index({ userId: 1 });
-  feedSchema.index({ groupId: 1 });
-  feedSchema.index({ status: 1 });
+  feedId: { type: String, required: true, unique: true },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  content: { type: String, required: true },
+  groupId: String,
+  images: [{ url: String, isAIGenerated: Boolean }],
+  status: { type: String, default: "active" },
+  commentCount: { type: Number, default: 0 },
+  // reactions를 subdocument 배열로 정의
+  reactions: [reactionSchema],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
 
+// 인덱스 추가
+feedSchema.index({ feedId: 1 });
+feedSchema.index({ userId: 1 });
+feedSchema.index({ groupId: 1 });
+feedSchema.index({ status: 1 });
 
-module.exports = mongoose.model('Feed', feedSchema);
+module.exports = mongoose.model("Feed", feedSchema);
